d4797d617e61f2e3c87399caf914558638dcb6a7
Salman Ali <salman.ali201518@gmail.com>
2022-11-05 22:57:28 -0700

    commit2

--- resources/views/append/board.blade.php
+++ resources/views/append/board.blade.php
@@ -157,6 +157,37 @@
                 }
             }
             $board_price = trim($board_price);
+              if ($board->title == 'Small'){
+                  if ($b_length * $for_multiply > 18){
+                      $board_price = ($board_price * 18) /  ($b_length * $for_multiply);
+                  }
+              }
+               if ($board->title == 'Medium'){
+                  if ($b_length * $for_multiply > 25){
+                      $board_price = ($board_price * 25) /  ($b_length * $for_multiply);
+                  }
+              }
+                if ($board->title == 'Large'){
+                  if ($b_length * $for_multiply > 35){
+                      $board_price = ($board_price * 35) /  ($b_length * $for_multiply);
+                  }
+              }
+                 if ($board->title == 'X Large'){
+                  if ($b_length * $for_multiply > 45){
+                      $board_price = ($board_price * 45) /  ($b_length * $for_multiply);
+                  }
+              }
+                  if ($board->title == 'XX Large'){
+                  if ($b_length * $for_multiply > 60){
+                      $board_price = ($board_price * 60) /  ($b_length * $for_multiply);
+                  }
+              }
+                   if ($board->title == 'Supersized'){
+                  if ($b_length * $for_multiply > 80){
+                      $board_price = ($board_price * 80) /  ($b_length * $for_multiply);
+                  }
+              }
+                   $board_price = trim($board_price);
     @endphp
 {{--    <p>{{json_encode($total_max_char_height_count)}}</p>--}}
 {{--    <p>{{json_encode($char_count_array)}}</p>--}}
@@ -164,14 +195,7 @@
     <div @if(isset($font_disable) && $font_disable == true) data-disable="yes" @else data-disable="not" @endif
     class="board @if($board_width != null && $board_length != null) @if($board_width == $board->width && $board_length == $board->length) active @endif @endif"
          data-length="{{$total_max_char_length_count}}" data-boardSize="{{$board->title}}" data-width="{{$total_max_char_height_count}}"
-         data-price="
-            @if($board->title == 'Small') @if(round($b_length * $for_multiply, 1) == 0 || round($b_height * $for_multiply , 1) == 0) 0 @else @if($b_price * $for_multiply  < 102) 102 @else @if($b_length * $for_multiply > 18) {{round((($b_price * $for_multiply) * 18) / ($b_length * $for_multiply) )}} @else {{round($b_price * $for_multiply)}} @endif @endif @endif @endif
-            @if($board->title == 'Medium') @if(round($b_length * $for_multiply, 1) == 0 || round($b_height * $for_multiply , 1) == 0) 0 @else @if($b_price * $for_multiply < 136) 136 @else @if($b_length * $for_multiply > 25) {{round((($b_price * $for_multiply) * 25) / ($b_length * $for_multiply) )}} @else {{round($b_price * $for_multiply)}} @endif @endif @endif @endif
-            @if($board->title == 'Large') @if(round($b_length * $for_multiply, 1) == 0 || round($b_height * $for_multiply , 1) == 0) 0 @else @if($b_price * $for_multiply < 180) 180 @else @if($b_length * $for_multiply > 35) {{round((($b_price * $for_multiply) * 35) / ($b_length * $for_multiply) )}} @else {{round($b_price * $for_multiply)}} @endif @endif @endif @endif
-            @if($board->title == 'X Large') @if(round($b_length * $for_multiply, 1) == 0 || round($b_height * $for_multiply , 1) == 0) 0 @else @if($b_price * $for_multiply < 223) 223 @else @if($b_length * $for_multiply > 47) {{round((($b_price * $for_multiply) * 47) / ($b_length * $for_multiply) )}} @else {{round($b_price * $for_multiply)}} @endif @endif @endif @endif
-            @if($board->title == 'XX Large') @if(round($b_length * $for_multiply, 1) == 0 || round($b_height * $for_multiply , 1) == 0) 0 @else @if($b_price * $for_multiply < 256) 256 @else @if($b_length * $for_multiply > 60) {{round((($b_price * $for_multiply) * 60) / ($b_length * $for_multiply) )}} @else {{round($b_price * $for_multiply)}} @endif @endif @endif @endif
-            @if($board->title == 'Supersized') @if(round($b_length * $for_multiply, 1) == 0 || round($b_height * $for_multiply , 1) == 0) 0 @else @if($b_price * $for_multiply < 301) 301 @else @if($b_length * $for_multiply > 80) {{round((($b_price * $for_multiply) * 80) / ($b_length * $for_multiply) )}} @else {{round($b_price * $for_multiply)}} @endif @endif @endif @endif
-         ">
+         data-price="{{$board_price}}">
         <div class="board_price"
              @if(isset($font_disable) && $font_disable == true) style=" width: auto !important;" @else @endif>
             {{ $board->title }}<br>
